090da2d7f72c7511da057ddcbce28426a4d6d2e1
Zand3rs <anders.j@me.com>
2026-03-15 16:21:24 +0100

    Update version codename check for Ubuntu 24.04 and 25

--- bundleInstaller.sh
+++ bundleInstaller.sh
@@ -22,7 +22,7 @@ fi
 # shellcheck disable=SC1091
 source /etc/os-release
 
-if [[ "${ID:-}" != "ubuntu" || ( "${VERSION_CODENAME:-}" != "noble" && "${VERSION_CODENAME:-}" != "oracular" ) ]]; then
+if [[ "${ID:-}" != "ubuntu" || ( "${VERSION_CODENAME:-}" != "noble" && "${VERSION_CODENAME:-}" != "plucky" ) ]]; then
   echo "This script is intended for Ubuntu 24.04 or Ubuntu 25."
   echo "Detected: ${PRETTY_NAME:-unknown}"
   exit 1
